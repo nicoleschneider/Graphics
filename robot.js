a9f03d7b8bb61a9afe4dfa80d73679684e671b54
nicoleschneider <nschneider@loyola.edu>
2018-12-09 19:20:48 -0500

    spotlight shadow works

--- robot.js
+++ robot.js
@@ -7,6 +7,9 @@ var renderer = new THREE.WebGLRenderer();
 
 renderer.gammaInput = true;
 renderer.gammaOutput = true;
+renderer.shadowCameraBottom = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.shadowMap.enabled = true;
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor('white');
@@ -20,9 +23,7 @@ var scene = new THREE.Scene();
 var axesHelper = new THREE.AxesHelper(10);
 scene.add(axesHelper);
 
-// ground plane grid
-var gridHelper = new THREE.GridHelper(10, 10);
-scene.add(gridHelper);
+
 
 // camera
 var aspect = window.innerWidth / window.innerHeight;
@@ -47,7 +48,7 @@ path + 'pz' + format, path + 'nz' + format];
 
 //var textureCube = new THREE.CubeTextureLoader().load( urls );
 
-scene = new THREE.Scene();
+//scene = new THREE.Scene();
 //scene.background = textureCube;
 
 var geometry = new THREE.SphereBufferGeometry( 0.1, 32, 16 );
@@ -58,7 +59,64 @@ var geometry = new THREE.SphereBufferGeometry( 0.1, 32, 16 );
 //scene.background = fogColor;
 //scene.fog = new THREE.Fog(fogColor, 0.0025, 20);
 
+// lights
+var light_ambient = new THREE.AmbientLight(0xffffff, 0.4);
+scene.add(light_ambient);
+var light_point = new THREE.PointLight();
+light_point.intensity = 0.6;
+light_point.position.set(camera.position.x+5, camera.position.y+5, camera.position.z+5);
+light_point.castShadow = true;
+light_point.shadow.camera.near = 0.1;       
+light_point.shadow.camera.far = 12000;      
+light_point.shadow.camera.left = -50;
+light_point.shadow.camera.bottom = -50;
+light_point.shadow.camera.right = 50;
+light_point.shadow.camera.top = 50;
+//light_point.shadow.mapSize.width = 2048;
+//light_point.shadow.mapSize.height = 2048;
+scene.add(light_point);
 
+// add directional light
+//https://stackoverflow.com/questions/15478093/realistic-lighting-sunlight-with-three-js
+var dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
+dirLight.position.set( 1, 0.75, 1 );
+dirLight.position.multiplyScalar( 50);
+dirLight.name = "dirlight";
+dirLight.shadowCameraVisible = true;
+dirLight.castShadow = true;
+dirLight.shadowMapWidth = dirLight.shadowMapHeight = 1024*2;
+
+dirLight.shadow.camera.near = 0.1;       
+dirLight.shadow.camera.far = 20;      
+dirLight.shadow.camera.left = -50;
+dirLight.shadow.camera.bottom = -50;
+dirLight.shadow.camera.right = 50;
+dirLight.shadow.camera.top = 50;
+
+var d = 300;
+
+//dirLight.shadowCameraLeft = -d;
+//dirLight.shadowCameraRight = d;
+//dirLight.shadowCameraTop = d;
+//dirLight.shadowCameraBottom = -d;
+
+//dirLight.shadowCameraFar = 350;
+//dirLight.shadowBias = -0.50001;
+dirLight.shadowDarkness = 0.65;
+//scene.add(dirLight);
+
+// add hemisphere light
+var hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.2 );
+hemiLight.position.set( 0, 500, 0 );
+scene.add( hemiLight );
+
+var spotLight = new THREE.SpotLight(0xffffff, 1);
+//var spotLight_01 = getSpotlight();
+spotLight.castShadow = true;
+scene.add(spotLight);
+spotLight.position.x = 6;
+spotLight.position.y = 8;
+spotLight.position.z = -20;
 
 // left arm
 var L_arm_geometry = new THREE.CylinderGeometry(0.25, 0.25, 3);
@@ -66,6 +124,7 @@ var L_arm_material = new THREE.MeshPhongMaterial();
 L_arm_material.color = new THREE.Color("black");
 var L_arm_mesh = new THREE.Mesh(L_arm_geometry, L_arm_material);
 L_arm_mesh.position.y = 3;
+L_arm_mesh.position.castShadow = true;
 
 // right arm
 var R_arm_geometry = new THREE.CylinderGeometry(0.25, 0.25, 3);
@@ -94,7 +153,7 @@ var torso_material = new THREE.MeshPhongMaterial( {
     specular: 0x050505,
     shininess: 500
 } ) 
-
+torso_material.castShadow = true;
 torso_material.color = new THREE.Color("silver");
 var torso_mesh = new THREE.Mesh(torso_geometry, torso_material);
 
@@ -111,6 +170,7 @@ L_leg_material.color = new THREE.Color("purple");
 var L_leg_mesh = new THREE.Mesh(L_leg_geometry, L_leg_material);
 L_leg_mesh.position.y = 1.5; // above ground
 L_leg_mesh.position.x = 1;
+L_leg_mesh.castShadow = true;
 var L_hip = new THREE.Group();
 L_hip.add(L_leg_mesh);
 
@@ -119,6 +179,7 @@ var R_leg_geometry = new THREE.BoxGeometry(1, 2.5, 1);
 var R_leg_material = new THREE.MeshStandardMaterial({roughness: 0.2, metalness: 0.5});
 R_leg_material.color = new THREE.Color("purple");
 var R_leg_mesh = new THREE.Mesh(R_leg_geometry, R_leg_material);
+R_leg_mesh.castShadow = true;
 R_leg_mesh.position.y = 1.5; // above ground
 R_leg_mesh.position.x = -1;
 
@@ -130,47 +191,26 @@ robot.add(body);
 
 scene.add(robot);
 
-// lights
-var light_ambient = new THREE.AmbientLight(0xffffff, 0.4);
-scene.add(light_ambient);
-var light_point = new THREE.PointLight();
-light_point.intensity = 0.6;
-light_point.position.set(camera.position.x, camera.position.y, camera.position.z);
-scene.add(light_point);
 
-// add directional light
-//https://stackoverflow.com/questions/15478093/realistic-lighting-sunlight-with-three-js
-var dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
-dirLight.position.set( -1, 0.75, 1 );
-dirLight.position.multiplyScalar( 50);
-dirLight.name = "dirlight";
-dirLight.shadowCameraVisible = true;
-dirLight.castShadow = true;
-dirLight.shadowMapWidth = dirLight.shadowMapHeight = 1024*2;
 
-var d = 300;
 
-dirLight.shadowCameraLeft = -d;
-dirLight.shadowCameraRight = d;
-dirLight.shadowCameraTop = d;
-dirLight.shadowCameraBottom = -d;
+// ground plane grid
+var gridHelper = new THREE.GridHelper(10, 10);
+scene.add(gridHelper);
+var planeGeometry = new THREE.PlaneBufferGeometry( 20, 20, 32, 32 );
+var planeMaterial = new THREE.MeshStandardMaterial( { color: 0x00ff00 } )
+var plane = new THREE.Mesh( planeGeometry, planeMaterial );
+//var geometry = new THREE.PlaneGeometry( 10, 10, 10 );
+//var material = new THREE.MeshBasicMaterial( {color: 0x000f000, side: THREE.DoubleSide} );
+//var plane = new THREE.Mesh( geometry, material );
+//plane.rotateX( - Math.PI / 2);
+plane.rotation.x -= Math.PI/2;
+plane.receiveShadow = true;
+scene.add( plane );
+// https://stackoverflow.com/questions/29916886/three-js-add-plane-to-the-scene
 
-dirLight.shadowCameraFar = 3500;
-dirLight.shadowBias = -0.0001;
-dirLight.shadowDarkness = 0.35;
-
-// add hemisphere light
-var hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.2 );
-hemiLight.position.set( 0, 500, 0 );
-scene.add( hemiLight );
-
-var spotLight = new THREE.SpotLight(0xffffff, 1);
-//var spotLight_01 = getSpotlight();
-scene.add(spotLight);
-spotLight.position.x = 6;
-spotLight.position.y = 8;
-spotLight.position.z = -20;
-
+var helper2 = new THREE.CameraHelper( dirLight.shadow.camera );
+scene.add( helper2 );
 
 // stats
 var stats = new Stats();
