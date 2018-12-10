0cf8fae3b4d2e0141182389c0974b00ee14d08f7
nicoleschneider <nschneider@loyola.edu>
2018-12-09 22:56:53 -0500

    cleaner

--- robot.js
+++ robot.js
@@ -1,6 +1,23 @@
 /**
 * Nicole Schneider
+* Graphics Final Project
 */
+
+// update position of light as camera moves
+function light_update()
+{
+    spotLight.position.copy( camera.position );
+	spotLight.position.y+=5;
+}
+
+function getShinyMaterial()
+{
+	return new THREE.MeshPhongMaterial( { 
+    color: 0x996633,
+    specular: 0x050505,
+    shininess: 500
+	} ) 
+}
 
 // renderer
 var renderer = new THREE.WebGLRenderer();
@@ -23,45 +40,19 @@ var scene = new THREE.Scene();
 var axesHelper = new THREE.AxesHelper(30);
 scene.add(axesHelper);
 
-
-
 // camera
 var aspect = window.innerWidth / window.innerHeight;
 var camera = new THREE.PerspectiveCamera(45, aspect, 0.1, 1000);
-camera.position.set(20.0, 15.0, 15.0);
+camera.position.set(20.0, 5.0, 15.0);
 
-
-
-
-
-var path = "C:/Users/Nicole/Desktop/Graphics/FINAL/background/";
-var format = '.png';
-
-//var loader = new THREE.ImageLoader();
-
-
-var urls = [
-path + 'px' + format, path + 'nx' + format,
-path + 'py' + format, path + 'ny' + format,
-path + 'pz' + format, path + 'nz' + format];
-//urls = "C:/Users/Nicole/Desktop/Graphics/FINAL/threejs/background.png";
-
-//var textureCube = new THREE.CubeTextureLoader().load( urls );
-
-//scene = new THREE.Scene();
-//scene.background = textureCube;
-
-var geometry = new THREE.SphereBufferGeometry( 0.1, 32, 16 );
-//var material = new THREE.MeshBasicMaterial( { color: 0xffffff, envMap: textureCube } );
-
-//fogColor = new THREE.Color(0xffffff);
- 
-//scene.background = fogColor;
-//scene.fog = new THREE.Fog(fogColor, 0.0025, 20);
 
 // lights
-var light_ambient = new THREE.AmbientLight(0xffffff, 0.4);
+
+// ambient light
+var light_ambient = new THREE.AmbientLight(0xffffff, 0.3);
 scene.add(light_ambient);
+
+// point light
 var light_point = new THREE.PointLight();
 light_point.intensity = 0.6;
 light_point.position.set(camera.position.x+5, camera.position.y+15, camera.position.z+5);
@@ -76,7 +67,7 @@ light_point.shadow.mapSize.width = 2048;
 light_point.shadow.mapSize.height = 2048;
 scene.add(light_point);
 
-// add directional light
+// directional light
 //https://stackoverflow.com/questions/15478093/realistic-lighting-sunlight-with-three-js
 var dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
 dirLight.position.set( 1, 0.75, 1 );
@@ -93,46 +84,27 @@ dirLight.shadow.camera.bottom = -50;
 dirLight.shadow.camera.right = 50;
 dirLight.shadow.camera.top = 50;
 
-var d = 300;
-
-//dirLight.shadowCameraLeft = -d;
-//dirLight.shadowCameraRight = d;
-//dirLight.shadowCameraTop = d;
-//dirLight.shadowCameraBottom = -d;
-
-//dirLight.shadowCameraFar = 350;
-//dirLight.shadowBias = -0.50001;
 dirLight.shadowDarkness = 0.65;
-/////////scene.add(dirLight);
+/////////scene.add(dirLight);/////////////////////////////////////////////////////
 
-// add hemisphere light
+// hemisphere light
 var hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.2 );
 hemiLight.position.set( 10, 500, 10 );
 scene.add( hemiLight );
 
+// spotlight
 var spotLight = new THREE.SpotLight(0xffffff, 1);
 spotLight.castShadow = true;
-
-spotLight.position.x = 6;
-spotLight.position.y = 18;
-spotLight.position.z = 20;
-
 spotLight.position.copy( camera.position );
-
-
 spotLight.shadow.camera.near = 0.5;
 spotLight.shadow.camera.far = 50.0;
-
-function light_update()
-{
-    spotLight.position.copy( camera.position );
-	spotLight.position.y+=5;
-}
 scene.add(spotLight);
+
+
 
 // left arm
 var L_arm_geometry = new THREE.CylinderGeometry(0.25, 0.25, 3);
-var L_arm_material = new THREE.MeshPhongMaterial();
+var L_arm_material = getShinyMaterial();
 L_arm_material.color = new THREE.Color("black");
 var L_arm_mesh = new THREE.Mesh(L_arm_geometry, L_arm_material);
 L_arm_mesh.position.y = 3;
@@ -140,20 +112,17 @@ L_arm_mesh.castShadow = true;
 
 // right arm
 var R_arm_geometry = new THREE.CylinderGeometry(0.25, 0.25, 3);
-var R_arm_material = new THREE.MeshPhongMaterial( { 
-    color: 0x996633,
-    specular: 0x050505,
-    shininess: 500
-} ) 
+var R_arm_material = getShinyMaterial();
 R_arm_material.color = new THREE.Color("black");
 var R_arm_mesh = new THREE.Mesh(R_arm_geometry, R_arm_material);
 R_arm_mesh.position.y = -3;
 R_arm_mesh.castShadow = true;
 
 
-var elevation = new THREE.Group();
-elevation.add(R_arm_mesh);
-elevation.add(L_arm_mesh);
+var R_shoulder = new THREE.Group();
+R_shoulder.add(R_arm_mesh);
+var L_shoulder = new THREE.Group();
+L_shoulder.add(L_arm_mesh);
 
 
 // body
@@ -174,7 +143,8 @@ torso_mesh.castShadow = true;
 var body = new THREE.Group();
 body.add(torso_mesh);
 body.rotation.z = 1.5*Math.PI;
-body.add(elevation);
+body.add(L_shoulder);
+body.add(R_shoulder);
 body.position.y = 4;
 
 // left leg
@@ -209,22 +179,19 @@ scene.add(robot);
 
 
 // ground plane grid
+// https://stackoverflow.com/questions/29916886/three-js-add-plane-to-the-scene
 var gridHelper = new THREE.GridHelper(20, 20);
 scene.add(gridHelper);
+
 var planeGeometry = new THREE.PlaneBufferGeometry( 40, 40, 32, 32 );
 var planeMaterial = new THREE.MeshStandardMaterial( { color: 0xadd8e6 } )
 var plane = new THREE.Mesh( planeGeometry, planeMaterial );
-//var geometry = new THREE.PlaneGeometry( 10, 10, 10 );
-//var material = new THREE.MeshBasicMaterial( {color: 0x000f000, side: THREE.DoubleSide} );
-//var plane = new THREE.Mesh( geometry, material );
-//plane.rotateX( - Math.PI / 2);
+
 plane.rotation.x -= Math.PI/2;
 plane.receiveShadow = true;
-scene.add( plane );
-// https://stackoverflow.com/questions/29916886/three-js-add-plane-to-the-scene
 
-var helper2 = new THREE.CameraHelper( dirLight.shadow.camera );
-scene.add( helper2 );
+scene.add( plane );
+
 
 // stats
 var stats = new Stats();
@@ -234,8 +201,9 @@ document.body.appendChild(stats.dom);
 var gui = new dat.GUI();
 var h = gui.addFolder("Pose Parameters");
 h.open();
-h.add(elevation.rotation, "z", -0.5*Math.PI, 0.5*Math.PI, 0.01).name("Elevation Angle");
-h.add(body.rotation, "y", 0.0, Math.PI, 0.01).name("Torso Angle");
+h.add(R_shoulder.rotation, "z", -0.25*Math.PI, 0.25*Math.PI, 0.01).name("Right Shoulder Angle");
+h.add(L_shoulder.rotation, "z", -0.25*Math.PI, 0.25*Math.PI, 0.01).name("Left Shoulder Angle");
+h.add(body.rotation, "y", -0.5*Math.PI, 0.5*Math.PI, 0.01).name("Torso Angle");
 
 // controls
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
